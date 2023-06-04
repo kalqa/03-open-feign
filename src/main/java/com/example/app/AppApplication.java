7c54e3a8d424d1888b64cf6bb97e2d59b2f685e8
bartlomiejkalka <bartlomiejkalqa@gmail.com>
2023-06-04 13:07:22 +0200

    handling exceptions feign

--- src/main/java/com/example/app/AppApplication.java
+++ src/main/java/com/example/app/AppApplication.java
@@ -1,5 +1,7 @@
 package com.example.app;
 
+import feign.FeignException;
+import feign.RetryableException;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
@@ -20,14 +22,24 @@ public class AppApplication {
     }
 
     @EventListener(ApplicationStartedEvent.class)
-    public void makeRequestToShawnMendesEndpoint(){
-        ShawnMendesResponse response = shawnMendesClient.makeSearchRequest("shawnmendes", 5);
-//        System.out.println(response);
-        List<ShawnMendesResult> results = response.results();
-        results.forEach(
-                shawnMendesResult ->
-                        System.out.println(shawnMendesResult.trackName())
-        );
+    public void makeRequestToShawnMendesEndpoint() {
+        try {
+            ShawnMendesResponse response = shawnMendesClient.makeSearchRequest("shawnmendes", 5);
+            List<ShawnMendesResult> results = response.results();
+            results.forEach(
+                    shawnMendesResult ->
+                            System.out.println(shawnMendesResult.trackName())
+            );
+        } catch (FeignException.FeignClientException feignException) {
+            System.out.println("client exception: " + feignException.status());
+        } catch (FeignException.FeignServerException feignException) {
+            System.out.println("server exception: " + feignException.status());
+        } catch (RetryableException retryableException) {
+            System.out.println("retryable exception " + retryableException.getMessage());
+        } catch (FeignException feignException) {
+            System.out.println(feignException.getMessage());
+            System.out.println(feignException.status());
+        }
     }
 
 }
--- src/main/java/com/example/app/ShawnMendesProxy.java
+++ src/main/java/com/example/app/ShawnMendesProxy.java
@@ -4,7 +4,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient(value = "shawnmendes-client", url = "https://itunes.apple.com")
+@FeignClient(value = "shawnmendes-client", url = "https://itunes.apple.com22")
 public interface ShawnMendesProxy {
 
     // GET https://itunes.apple.com/search?term=shawnmendes&limit=1
